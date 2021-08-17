5634f5d31a4d68b3eb68e0c9779fa97aa5c3e1db
Joao Stephan <joaossmauricio@gmail.com>
2021-08-16 23:07:45 -0300

    Tests a diferent occurence of leftrotation

--- main.cpp
+++ main.cpp
@@ -1,42 +1,19 @@
 #include <iostream>
 
-#include "BST.h"
+#include "RB.h"
 
 using namespace std;
 
-//TODO min & max
-//TODO other ways to print the tree 
-
 int main() {
-  cout << "Binary Search Tree\n" << "Provide the keys you want to insert or -1 to leave insertion:\n";
-  BST<int> bst;
-  int key;
-  cin >> key;
-  while(key != -1) {
-    bst.insertBST(key);
-    cin >> key;
-  }
-  bst.print();
-  cout << "Provide the keys you want to delete or -1 to leave deletion:\n";
-  cin >> key;
-  while(key != -1) {
-    bst.deleteBST(key);
-    //bst.print();
-    cin >> key;
-  }
-  /*BST<int> bst;
-  bst.insertBST(20);
-  bst.insertBST(10);
-  bst.insertBST(30);
-  bst.insertBST(8);
-  bst.insertBST(15);
-  bst.insertBST(25);
-  bst.insertBST(33);
-  bst.insertBST(6);
-  bst.insertBST(9);
-  bst.insertBST(14);
-  //bst.insertBST(18);
-  bst.print();
-  bst.deleteBST(15);
-  //bst.print();*/
+  /*const string red("\033[0;31;47m");
+  const string black("\033[0;30;47m");
+  const string reset("\033[0m");
+  cout << black << "text" << reset << endl;
+  cout << "normal?" << endl;*/
+  RB<int> flamengo;
+  //flamengo.insertRB(10);
+  flamengo.insertRB(30);
+  flamengo.insertRB(70);
+  flamengo.insertRB(88);
+  flamengo.print();
 }
