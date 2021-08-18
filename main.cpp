a4a79a0518e879f5a8bddebc397965895fdf3cbb
Joao Stephan <joaossmauricio@gmail.com>
2021-08-18 17:49:32 -0300

    Test with 20 random integers

--- main.cpp
+++ main.cpp
@@ -5,22 +5,9 @@
 using namespace std;
 
 int main() {
-  /*const string red("\033[0;31;47m");
-  const string black("\033[0;30;47m");
-  const string reset("\033[0m");
-  cout << black << "text" << reset << endl;
-  cout << "normal?" << endl;*/
   RB<int> flamengo;
-  //flamengo.insertRB(10);
-  flamengo.insertRB(1);
-  flamengo.insertRB(2);
-  flamengo.insertRB(3);
-  flamengo.insertRB(4);
-  flamengo.insertRB(5);
-  flamengo.insertRB(6);
-  flamengo.insertRB(7);
-  //flamengo.insertRB(8);
-  //flamengo.insertRB(9);
+  for (int i = 0; i < 20 ; i++)
+    flamengo.insertRB(rand()%98 + 1);
   
   flamengo.print();
 }
